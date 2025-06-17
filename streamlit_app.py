f1858e49cefa2a107015980f98c65352c70769dc
Lukas Masuch <Lukas.Masuch@gmail.com>
2025-06-17 00:47:04 +0000

    Add extra

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
-
+from streamlit_extras.pdf_viewer import pdf_viewer
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+pdf_viewer(
+        "https://pdfobject.com/pdf/sample.pdf",
+    )
