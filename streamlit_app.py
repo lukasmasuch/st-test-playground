c136900db3c53692ba514c3a338584400a9d8d68
Lukas Masuch <Lukas.Masuch@gmail.com>
2025-06-19 14:14:07 +0000

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,8 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
+st.markdown("[![Click me](app/static/example.jpg)](https://streamlit.io)")
+
 
 html_content = f"""
 <iframe
@@ -16,3 +18,14 @@ html_content = f"""
 """
 
 st.markdown(html_content, unsafe_allow_html=True)
+
+html_content_2 = f"""
+<iframe
+    src= "app/static/sample.pdf",
+    style="width: 100%; height: 500px"
+    type="application/pdf"
+    frameborder="0"
+></iframe>
+"""
+
+st.markdown(html_content_2, unsafe_allow_html=True)
